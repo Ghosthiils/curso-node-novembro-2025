66d63a5aa7ec6a4a252b3388542e09176edc91e7
Luiz Fernando <luizfranca35mc@gmail.com>
2025-11-07 22:28:50 -0300

    API REST

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Ola mundo")
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.get("/pt", function (req, res) {
+  res.send("ola mundo");
+});
+
+app.get("/es", function (req, res) {
+  res.send("Hola");
+});
+
+app.get("/en", function (req, res) {
+  res.send("hello");
+});
+
+app.listen(3000);
